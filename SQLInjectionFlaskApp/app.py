df20f2f01fdff3a89a27c06712e997c3e2298d96
Shuvam Ghosh <jhux11ghosh@gmail.com>
2021-04-19 20:52:22 -0700

    Sql injection error handling (#1)
    * Added error handling for sql injection to prevent server crash
    
    * Added error handling for sql injection to prevent server crash

--- SQLInjectionFlaskApp/app.py
+++ SQLInjectionFlaskApp/app.py
@@ -2,46 +2,54 @@ from flask import Flask, redirect, url_for, request, render_template
 from pymysql import connect, cursors
 import subprocess
 
+
 def restart_mysql_service():
-   command = 'service mysql restart'
-   process = subprocess.Popen(command.split(), stdout=subprocess.PIPE)
-   output, error = process.communicate()
+    command = 'service mysql restart'
+    process = subprocess.Popen(command.split(), stdout=subprocess.PIPE)
+    output, error = process.communicate()
+
 
 app = Flask(__name__)
 
 restart_mysql_service()
 
+
 @app.route('/success/<name>')
 def success(name):
-   return 'welcome %s' % name
+    return 'welcome %s' % name
+
 
 @app.route('/')
 def search_inventory():
-   return render_template('search_inventory.html')
+    return render_template('search_inventory.html', search_result=[{}])
 
-@app.route('/submit',methods = ['POST', 'GET'])
+
+@app.route('/submit', methods=['POST', 'GET'])
 def submit():
-   result=''
-   item = request.form['item']
-   if request.method == 'POST':
-      
-      connection = connect(host='localhost',
-                           user='root',
-                           password='mysqlroot',
-                           database='inventory',
-                           cursorclass=cursors.DictCursor)
-      with connection:
-         with connection.cursor() as cursor:
-            # select * from items where name like '%hammer' UNION (SELECT TABLE_NAME, TABLE_SCHEMA FROM information_schema.tables);-- %'
-            # hammer' UNION (SELECT TABLE_NAME, TABLE_SCHEMA FROM information_schema.tables);-- 
-            sql_command = "select * from items where item_name like '%%"+item+"%'"
-            cursor.execute(sql_command)
-            result = cursor.fetchall()
-               
-      
-      return render_template('search_inventory.html', user=result)
-   else:
-      return render_template('search_inventory.html')
+    result = ''
+    item = request.form['item']
+    if request.method == 'POST':
+        try:
+            connection = connect(host='',
+                                 user='root',
+                                 password='mysqlroot',
+                                 database='inventory',
+                                 cursorclass=cursors.DictCursor)
+            with connection:
+                with connection.cursor() as cursor:
+                    # select * from items where name like '%hammer' UNION (SELECT TABLE_NAME, TABLE_SCHEMA FROM information_schema.tables);-- %'
+                    # hammer' UNION (SELECT TABLE_NAME, TABLE_SCHEMA FROM information_schema.tables);--
+                    sql_command = "select * from items where item_name like '%%"+item+"%'"
+                    cursor.execute(sql_command)
+                    result = cursor.fetchall()
+        except Exception as e:
+            return render_template('search_inventory.html', search_result=[{"error": str(e)}])
+
+        return render_template('search_inventory.html', search_result=result)
+    else:
+        return render_template('search_inventory.html', search_result=[{}])
+
 
 if __name__ == '__main__':
-   app.run(host="0.0.0.0",port=5000,debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
+
