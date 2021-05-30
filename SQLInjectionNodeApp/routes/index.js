a145b5c4d4fcfd1e974ca377cf70dc16da7fb07a
Maaz Syed Adeeb <maaz.adeeb@gmail.com>
2021-05-30 12:43:44 -0700

    feat: node sql injection cors

--- SQLInjectionNodeApp/routes/index.js
+++ SQLInjectionNodeApp/routes/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const cors = require('cors');
+
 const router = express.Router();
 const { getSearchResults } = require('./db');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  return res.render('index', {search_result: [{}]});
+router.get('/', function (req, res, next) {
+  return res.render('index', { search_result: [{}] });
 });
 
 router.all('/submit', async (req, res, next) => {
@@ -12,14 +14,15 @@ router.all('/submit', async (req, res, next) => {
     const searchRes = await getSearchResults(req.body.item);
     return res.render('index', searchRes);
   } catch (error) {
-    return res.render('index', { search_result: [{ error } ]});
+    return res.render('index', { search_result: [{ error }] });
   }
 });
 
-router.get('/status', async (req, res, next) => {
+router.get('/status', cors(), async (req, res, next) => {
   const status = { solved: false };
   try {
-    const item = "hammer' UNION (SELECT TABLE_NAME, TABLE_SCHEMA FROM information_schema.tables);-- ";
+    const item =
+      "hammer' UNION (SELECT TABLE_NAME, TABLE_SCHEMA FROM information_schema.tables);-- ";
     const { search_result } = await getSearchResults(item);
     status.solved = Array.isArray(search_result) && !search_result.length;
     return res.json(status);
